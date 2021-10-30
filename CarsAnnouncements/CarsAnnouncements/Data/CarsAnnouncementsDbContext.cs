7b4d6cf815d15fcce86b1db713affa0b717cface
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-30 16:55:08 +0300

    Add database sets

--- CarsAnnouncements/CarsAnnouncements/Data/CarsAnnouncementsDbContext.cs
+++ CarsAnnouncements/CarsAnnouncements/Data/CarsAnnouncementsDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarsAnnouncements.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarsAnnouncements.Data
 {
@@ -7,6 +8,40 @@ namespace CarsAnnouncements.Data
         public CarsAnnouncementsDbContext(DbContextOptions options)
             : base(options)
         {
+        }
+
+        public DbSet<Car> Cars { get; init; }
+
+        public DbSet<Brand> Brands { get; init; }
+
+        public DbSet<Fuel> Fuels { get; init; }
+
+        public DbSet<Transmission> Transmissions { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<Car>()
+                .HasOne(c => c.Brand)
+                .WithMany(b => b.Cars)
+                .HasForeignKey(c => c.BrandId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Car>()
+                .HasOne(c => c.Fuel)
+                .WithMany(f => f.Cars)
+                .HasForeignKey(c => c.FuelId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .Entity<Car>()
+                .HasOne(c => c.Transmission)
+                .WithMany(t => t.Cars)
+                .HasForeignKey(c => c.TransmissionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
         }
     }
 }
