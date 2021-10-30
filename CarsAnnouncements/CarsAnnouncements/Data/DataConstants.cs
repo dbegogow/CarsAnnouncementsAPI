e2e94388534dac19301fc018cd5d721e25d5f4b7
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-30 17:36:03 +0300

    Add more data constants

--- CarsAnnouncements/CarsAnnouncements/Data/DataConstants.cs
+++ CarsAnnouncements/CarsAnnouncements/Data/DataConstants.cs
@@ -6,5 +6,14 @@
         public const int NameMaxLength = 30;
 
         public const int YearMinValue = 1950;
+
+        public const int PriceMinValue = 100;
+        public const int PriceMaxValue = 1500000;
+
+        public const int DoorsMinValue = 2;
+        public const int DoorsMaxValue = 6;
+
+        public const int HorsepowerMinValue = 20;
+        public const int HorsepowerMaxValue = 1500;
     }
 }
