16c9c7e4a9133360ee016ae99b553bbcf31c94a2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-11-15 11:29:32 +0200

    Implement cars controller with get all action method

--- CarsAnnouncements/CarsAnnouncements/Controllers/CarsController.cs
+++ CarsAnnouncements/CarsAnnouncements/Controllers/CarsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CarsAnnouncements.Models.Cars;
+using CarsAnnouncements.Services.Cars;
 
 namespace CarsAnnouncements.Controllers
 {
@@ -6,10 +8,28 @@ namespace CarsAnnouncements.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+        private readonly ICarsService _cars;
+
+        public CarsController(ICarsService cars)
+            => this._cars = cars;
+
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(CarFilterModel model)
         {
-            return Ok();
+            var cars = this._cars
+                .All(
+                    model.Brand,
+                    model.Model,
+                    model.Type,
+                    model.Fuel,
+                    model.Transmission,
+                    model.Price,
+                    model.Year,
+                    model.Doors,
+                    model.Horsepower
+                );
+
+            return Ok(cars);
         }
     }
 }
---
+++ CarsAnnouncements/CarsAnnouncements/Models/Cars/CarFilterModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CarsAnnouncements.Models.Cars
+{
+    public class CarFilterModel
+    {
+        public int Id { get; init; }
+
+        public string Brand { get; init; }
+
+        public string Model { get; init; }
+
+        public string Type { get; init; }
+
+        public string Fuel { get; init; }
+
+        public string Transmission { get; init; }
+
+        public int? Price { get; init; }
+
+        public int? Year { get; init; }
+
+        public int? Doors { get; init; }
+
+        public int? Horsepower { get; init; }
+    }
+}
